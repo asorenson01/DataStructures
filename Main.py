14304805ab0766b33ee11bf942b2e342eaf64bf0
unknown <asorenson01@hotmail.com>
2021-04-26 19:13:08 -0400

    link list append and printing link list data working

--- Main.py
+++ Main.py
@@ -24,4 +24,12 @@ if __name__ == '__main__':
     linked_list.append_node(55)
     linked_list.append_node(60)
     linked_list.append_node(65)
+    linked_list.append_node(70)
+    linked_list.append_node(75)
+    linked_list.append_node(80)
+    linked_list.add_to_beginning(30)
+    linked_list.contains_node()
+
+    print("Hello World")
+
 
--- linkedlist.py
+++ linkedlist.py
@@ -4,19 +4,27 @@ from node import Node
 class LinkedList:
     def __init__(self):
         self.head = None
-
+        self.tail = None
 
     def append_node(self, data):
         node = Node(data)
 
         if self.head is None:
             self.head = node
+            self.tail = node
             return
+        else:
+            self.tail.next = node
+            self.tail = self.tail.next
 
-        temporary_node = self.head
+    def add_to_beginning(self, data):
+        node = Node(data)
+        node.next = self.head
+        self.head = node
+        print("hello world")
 
-        while temporary_node.next is not None:
-            temporary_node = temporary_node.next
-
-        temporary_node.next = node
-
+    def contains_node(self):
+        x = self.head
+        while x.next:
+            print(f'{x.data}')
+            x = x.next
