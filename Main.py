273906d9c4d5b2e173c2a2daba09c4f7394c4673
unknown <asorenson01@hotmail.com>
2021-04-26 14:17:26 -0400

    B Day locations SET comp

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
 from monthsoftheyear import Monthsoftheyear
+from bdaylocations import Bdaylocations
 
 if __name__ == '__main__':
-    test = Monthsoftheyear()
+    test1 = Monthsoftheyear()
     # test.print_moy()
-    test.find_month(3)
+    # test1.find_month(3)
+    test2 = Bdaylocations()
+    # test2.print_locations()
+    # test2.add_bday_locations("Skippers", "Chuck E Cheese", "Strip Club")
+    # test2.print_locations()
 
---
+++ bdaylocations.py
@@ -0,0 +1,11 @@
+class Bdaylocations:
+    def __init__(self):
+        self.locations = {"home", "Red Robbin", "Iraq", "Afghanistan", "In a Helicopter"}
+
+    def add_bday_locations(self, location1, location2, location3):
+        self.locations.update({location1, location2, location3})
+        print("<---------- ADDED 3 Locations------------>")
+
+    def print_locations(self):
+        for loc in self.locations:
+            print(loc)
